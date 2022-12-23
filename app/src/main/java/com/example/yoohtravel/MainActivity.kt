f0669531718ba72f8793ee79331d534fae90fc3b
Elruch <lenx-20@rambler.ru>
2022-12-23 11:34:23 +0300

    Добавлен тулбар Обновлены картинки регистрации

--- app/src/main/java/com/example/yoohtravel/MainActivity.kt
+++ app/src/main/java/com/example/yoohtravel/MainActivity.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -22,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.title = "Как туда попасть?"
+
 
         //Отображение фрагмент-окон через NavBar
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
@@ -38,6 +44,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, PhoneActivity::class.java))
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tool_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+            R.id.search_toolbar ->{
+                Toast.makeText(this,"Поиск(в разработке)", Toast.LENGTH_SHORT).show()
+            }
+            R.id.settings_toolbar ->{
+                Toast.makeText(this,"Настройки(в разработке)", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 }
 
